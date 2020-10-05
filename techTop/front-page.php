185fa8b6156f8f5b50495bc6effc307a83e1d649
Maxim Nepomniashchiy <maxim@modzi.team>
2020-10-05 09:08:00 +0300

    fixes

--- techTop/front-page.php
+++ techTop/front-page.php
@@ -7,9 +7,8 @@
                 <div class="columns is-mobile">
                     <div class="column is-10 home-slider-title">
                         <?php echo apply_filters( 'the_content', $item['crb_ts_description'] ); ?>
-                        <?php if (!is_wp_error(get_term_link((int)$item['crb_ts_category'][0]['id'], 'product_cat'))): ?>
-                            <a href="<?php echo get_term_link((int)$item['crb_ts_category'][0]['id'], 'product_cat'); ?>" class="btn btn-blue">צפה בקטלוג</a>
-                        <?php endif; ?>
+
+                        <a href="<?php echo get_term_link(get_term($item['crb_ts_category'][0]['id'])); ?>" class="btn btn-blue">צפה בקטלוג</a>
                     </div>
                     <div class="column is-2 col-products">
                         <span><span><?php echo $item['crb_ts_product_count']?></span>מוצרים</span>
@@ -87,6 +86,9 @@
             <h3>מוצרים לפי קטגוריה</h3>
         </div>
     </div>
+    <div class="container">
+        <div class="notification-area" style="margin-bottom: 20px;"></div>
+    </div>
     <?php
     $top_categories = get_categories( [
 	    'taxonomy'     => 'product_cat',
@@ -95,7 +97,6 @@
 	    'hide_empty'   => 1,
 	    'parent'       => 0
     ]);
-    $top_categories = [];
     $child_categories = [];
     foreach ($top_categories as $index => $tc) {
 	    if ($tc->term_id == 15)
@@ -110,6 +111,7 @@
 	    $child_categories[$tc->term_id][] = $cat;
     }
     ?>
+
     <?php foreach ($top_categories as $index => $parent_category): ?>
         <div class="home-category-item">
             <div class="container">
@@ -134,9 +136,39 @@
                         <?php foreach ($products as $product): ?>
                             <div class="catalog-products-item">
                                 <div class="item-wrap">
-                                    <a href="<?php echo get_permalink($product->get_ID()); ?>" class="item-img">
-                                        <img src="<?php echo get_post_meta($product->get_ID(), 'remote_image', true); ?>">
-                                    </a>
+                                    <div class="item-img">
+                                        <a href="<?php echo get_permalink($product->get_ID()); ?>" class="item-img">
+                                            <img src="<?php echo get_post_meta($product->get_ID(), 'remote_image', true); ?>">
+                                        </a>
+                                        <div class="btn-group">
+                                        <?php
+                                                if ($product->get_stock_quantity() > 0 && is_user_logged_in()) {
+                                                    echo apply_filters(
+                                                        'woocommerce_loop_add_to_cart_link',
+                                                        sprintf(
+                                                            '<a href="%s" data-product_id="%s" data-product_sku="%s" class="btn btn-blue %s product_type_%s add_to_cart_button ajax_add_to_cart">%s</a>',
+                                                            esc_url( $product->add_to_cart_url() ),
+                                                            esc_attr( $product->get_id() ),
+                                                            esc_attr( $product->get_sku() ),
+                                                            $product->is_purchasable() ? 'add_to_cart_button' : '',
+                                                            esc_attr( $product->product_type ),
+                                                            esc_html( $product->add_to_cart_text() )
+                                                        ),
+                                                        $product
+                                                    );
+                                                } elseif(!is_user_logged_in()) { ?>
+                                                    <a href="<?php echo home_url('resellers'); ?>"class="btn btn-blue ">להוסיף לתיק</a>
+                                               <?php }
+                                        ?>
+                                        <a href="#" class="like" data-sku="<?php echo $product->get_sku(); ?>">
+                                            <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M5.89757 2.33031C8.83939 2.33119 10.4185 5.41921 11.0976 6.78183C11.7794 5.41301 13.3421 2.33915 16.303 2.33915C18.1739 2.33915 20.1885 3.49738 20.1885 6.03947C20.1885 9.08501 15.8758 12.9862 11.0976 17.5421C6.31757 12.9844 2.00666 9.08413 2.00666 6.03947C2.00666 3.67169 3.79302 2.32942 5.89757 2.33031ZM5.89848 0.560669C3.00484 0.560669 0.188477 2.49577 0.188477 6.03947C0.188477 10.1636 5.25211 14.3807 11.0976 20.0267C16.943 14.3807 22.0067 10.1636 22.0067 6.03947C22.0067 2.49046 19.1912 0.569517 16.303 0.569517C14.2994 0.569517 12.2612 1.4915 11.0976 3.43456C9.92939 1.48265 7.89575 0.560669 5.89848 0.560669Z" fill="#767676"></path>
+                                                <path d="M5.89359 0.244507C2.99996 0.244507 0.183594 2.23269 0.183594 5.8736C0.183594 10.1109 5.24723 14.4436 11.0927 20.2445C16.9381 14.4436 22.0018 10.1109 22.0018 5.8736C22.0018 2.22723 19.1863 0.253598 16.2981 0.253598C14.2945 0.253598 12.2563 1.20087 11.0927 3.19723C9.9245 1.19178 7.89087 0.244507 5.89359 0.244507Z" fill="#C2D7F8" class="full-fill"></path>
+                                            </svg>
+                                        </a>
+                                    </div>
+                                    </div>
+
                                     <div class="item-info">
                                         <p class="title"><a href="<?php echo get_permalink($product->get_ID()); ?>"><?php echo $product->get_name(); ?> </a></p>
                                         <div class="item-flex">
